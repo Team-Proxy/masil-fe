ae63c5db43dc87e3507880cbb39e684f700358e3
kimeodml <kimeodml@gmail.com>
2026-01-17 14:21:08 +0900

    design: 닉네임 UI 추가

--- src/app/(public)/sign-up/nickname/page.tsx
+++ src/app/(public)/sign-up/nickname/page.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+
+import { KeyboardAwareButton } from '@/components/keyboard-aware-button/KeyboardAwareButton';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 export default function Home() {
-  return <div>닉네임</div>;
+  return (
+    <div>
+      <h1 className="pb-6">넥네임을 설정해 주세요.</h1>
+      <p className="text-muted-foreground pb-4 text-sm">
+        프로필에 표시되는 닉네임입니다.
+        <br />
+        저장 후에는 변경할 수 없습니다.
+      </p>
+      <div className="flex gap-2">
+        <Input type="text" placeholder="닉네임 입력하세요" />
+        <Button>중복 확인</Button>
+      </div>
+      <KeyboardAwareButton>
+        <Link href="/sign-up/birth">
+          <Button className="w-full">다음</Button>
+        </Link>
+      </KeyboardAwareButton>
+    </div>
+  );
 }
---
+++ src/components/progress-bar/ProgressBar.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+import { PreviousButton } from '@/components/previous-button/PreviousButton';
+import { Progress } from '@/components/ui/progress';
+
+const STEP_MAP: Record<string, number> = {
+  '/sign-up/email': 1,
+  '/sign-up/nickname': 2,
+  '/sign-up/birth': 3,
+  '/sign-up/profile-basic': 4,
+  '/sign-up/marital-status': 5,
+  '/sign-up/marital-status/children': 5,
+  '/sign-up/distance': 6,
+  '/sign-up/values/1': 7,
+  '/sign-up/values/2': 7,
+  '/sign-up/values/3': 7,
+  '/sign-up/values/4': 7,
+  '/sign-up/profile-image': 8,
+};
+
+export function ProgressBar() {
+  const pathname = usePathname();
+  const currentStep = STEP_MAP[pathname] || 0;
+  const totalStep = 8;
+
+  return (
+    <div>
+      <Progress value={(currentStep / totalStep) * 100} className="rounded-none" />
+      <div className="text-muted-foreground flex items-center justify-between">
+        <span>
+          <PreviousButton />
+        </span>
+        <span className="pr-3">
+          {currentStep} / {totalStep}
+        </span>
+      </div>
+    </div>
+  );
+}
