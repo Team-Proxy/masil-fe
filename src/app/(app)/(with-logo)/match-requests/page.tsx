edca9d83e277b5e0f99e5e62d0d4f15ac6cc0d09
kimeodml <kimeodml@gmail.com>
2026-01-25 17:33:05 +0900

    feat: 공용 컴포넌트 TabsLayout 추가

--- src/app/(app)/(with-logo)/match-requests/page.tsx
+++ src/app/(app)/(with-logo)/match-requests/page.tsx
@@ -1,3 +1,29 @@
+import TabsLayout from '@/components/TabsLayout/TabsLayout';
+
 export default function Page() {
-  return <div>매칭 상태 페이지</div>;
+  return (
+    <div>
+      <section className="px-4">
+        <h1 className="pb-2 text-lg font-semibold">나의 인연 현황</h1>
+        <p className="text-muted-foreground pb-4 text-sm">도착한 인연과 내가 보낸 인연 요청을 확인할 수 있습니다.</p>
+      </section>
+
+      <TabsLayout
+        defaultValue="received"
+        tabs={[
+          {
+            value: 'received',
+            label: '도착한 인연',
+            badgeCount: 30,
+            content: <div>몇 명 있음</div>,
+          },
+          {
+            value: 'sent',
+            label: '요청한 인연',
+            content: <div>몇 명 있음22</div>,
+          },
+        ]}
+      />
+    </div>
+  );
 }
