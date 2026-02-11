8fcc5cbf4c435b2b863d32cee524e49b700ebdda
kimeodml <kimeodml@gmail.com>
2026-02-11 21:09:48 +0900

    design: 생활 습관, 커뮤니케이션 성향, 여가 생활 UI 추가

--- src/app/(app)/(with-logo)/mypage/page.tsx
+++ src/app/(app)/(with-logo)/mypage/page.tsx
@@ -1,14 +1,10 @@
 import { PlusIcon } from 'lucide-react';
+import Link from 'next/link';
 
 import { Avatar, AvatarBadge, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
-import {
-  Popover,
-  PopoverContent,
-  PopoverDescription,
-  PopoverTrigger,
-} from '@/components/ui/popover';
+import { Popover, PopoverContent, PopoverDescription, PopoverTrigger } from '@/components/ui/popover';
 import { Progress } from '@/components/ui/progress';
 
 const VALUES_1 = [
@@ -20,6 +16,30 @@ const VALUES_1 = [
   { key: '직업', label: '요식업' },
   { key: '종교', label: '무교' },
   { key: '정치', label: '보수적인 편' },
+];
+
+const VALUES_2 = [
+  { key: '생활 리듬', label: '유동적' },
+  { key: '식사', label: '배달' },
+  { key: '소비 성향', label: '소비형' },
+  { key: '여향 스타일', label: ['국내'] },
+  { key: '생활 형태', label: '혼자' },
+  { key: '가족과의 관계', label: '명절 위주' },
+];
+
+const VALUES_3 = [
+  { key: '대화 스타일', label: '배려하는' },
+  { key: '갈등 해결 방식', label: '시간 둔 후 해결' },
+  { key: '연락 스타일', label: '필요할 때 연락' },
+  { key: '감정 표현 방식', label: '주 1-2회' },
+];
+
+const VALUES_4 = [
+  { key: '운동', label: ['런닝', '축구'] },
+  { key: '문화생활', label: ['책 읽기'] },
+  { key: '음악', label: ['KPOP'] },
+  { key: '취미', label: ['주 1-2회'] },
+  { key: '여행/나들이', label: ['국내 여행'] },
 ];
 
 export default function Page() {
@@ -40,13 +60,13 @@ export default function Page() {
           </div>
         </div>
 
-        <div className="border-primary/20 bg-primary/5 rounded-md border-2 p-4">
+        <div className="border-primary/10 bg-primary/5 rounded-md border-2 p-4">
           <div>
             <div className="flex justify-between">
-              <span className="font-semibold">내 정보 완성도 85%</span>
+              <span className="font-semibold">내 정보 완성도 80%</span>
               <span className="text-muted-foreground text-sm">80% 이상 매칭 확률 급증!</span>
             </div>
-            <Progress value={0.85 * 100} />
+            <Progress value={0.8 * 100} />
           </div>
         </div>
       </section>
@@ -56,27 +76,138 @@ export default function Page() {
           <br />
           카테고리별로 정보를 입력해주세요.
         </p>
-        <div className="flex flex-col gap-2">
-          <div className="flex items-center justify-between">
-            <h3 className="font-semibold">기본 프로필</h3>
-            <Button variant="outline" size="sm" className="border-primary/20 text-primary hover:text-none rounded-full">
-              수정
-            </Button>
+
+        <div className="flex flex-col gap-4">
+          <div className="border-primary/10 bg-primary/3 flex flex-col gap-2 rounded-3xl border p-4">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <h3 className="font-semibold">기본 프로필</h3>
+                <span className="text-muted-foreground text-xs">8/8</span>
+              </div>
+              <Link href="/signup/values/2">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="border-primary/20 text-primary hover:text-none rounded-full"
+                >
+                  수정
+                </Button>
+              </Link>
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              {VALUES_1.map(({ key, label }, index) => (
+                <Popover key={`${key}-${index}`}>
+                  <PopoverTrigger asChild>
+                    <Badge variant="secondary" className="cursor-pointer px-2 text-base">
+                      {label}
+                    </Badge>
+                  </PopoverTrigger>
+                  <PopoverContent align="center" side="top" className="w-fit p-1.5 text-xs">
+                    <PopoverDescription>{key}</PopoverDescription>
+                  </PopoverContent>
+                </Popover>
+              ))}
+            </div>
           </div>
 
-          <div className="flex flex-wrap gap-2">
-            {VALUES_1.map(({ key, label }, index) => (
-              <Popover key={`${key}-${index}`}>
-                <PopoverTrigger asChild>
-                  <Badge variant="secondary" className="cursor-pointer px-2 text-base">
-                    {label}
-                  </Badge>
-                </PopoverTrigger>
-                <PopoverContent align="center" side="top" className="w-fit p-1.5 text-xs">
-                  <PopoverDescription>{key}</PopoverDescription>
-                </PopoverContent>
-              </Popover>
-            ))}
+          <div className="border-primary/10 bg-primary/3 flex flex-col gap-2 rounded-3xl border p-4">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <h3 className="font-semibold">생활 습관</h3>
+                <span className="text-muted-foreground text-xs">6/6</span>
+              </div>
+              <Link href="/signup/values/2">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="border-primary/20 text-primary hover:text-none rounded-full"
+                >
+                  수정
+                </Button>
+              </Link>
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              {VALUES_2.map(({ key, label }, index) => (
+                <Popover key={`${key}-${index}`}>
+                  <PopoverTrigger asChild>
+                    <Badge variant="secondary" className="cursor-pointer px-2 text-base">
+                      {Array.isArray(label) ? label.join(' · ') : label}
+                    </Badge>
+                  </PopoverTrigger>
+                  <PopoverContent align="center" side="top" className="w-fit p-1.5 text-xs">
+                    <PopoverDescription>{key}</PopoverDescription>
+                  </PopoverContent>
+                </Popover>
+              ))}
+            </div>
+          </div>
+
+          <div className="border-primary/10 bg-primary/3 flex flex-col gap-2 rounded-3xl border p-4">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <h3 className="font-semibold">커뮤니케이션 성향</h3>
+                <span className="text-muted-foreground text-xs">4/4</span>
+              </div>
+              <Link href="/signup/values/2">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="border-primary/20 text-primary hover:text-none rounded-full"
+                >
+                  수정
+                </Button>
+              </Link>
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              {VALUES_3.map(({ key, label }, index) => (
+                <Popover key={`${key}-${index}`}>
+                  <PopoverTrigger asChild>
+                    <Badge variant="secondary" className="cursor-pointer px-2 text-base">
+                      {label}
+                    </Badge>
+                  </PopoverTrigger>
+                  <PopoverContent align="center" side="top" className="w-fit p-1.5 text-xs">
+                    <PopoverDescription>{key}</PopoverDescription>
+                  </PopoverContent>
+                </Popover>
+              ))}
+            </div>
+          </div>
+
+          <div className="border-primary/10 bg-primary/3 flex flex-col gap-2 rounded-3xl border p-4">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <h3 className="font-semibold">여가 생활</h3>
+                <span className="text-muted-foreground text-xs">5/8</span>
+              </div>
+              <Link href="/signup/values/2">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="border-primary/20 text-primary hover:text-none rounded-full"
+                >
+                  수정
+                </Button>
+              </Link>
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              {VALUES_4.map(({ key, label }, index) => (
+                <Popover key={`${key}-${index}`}>
+                  <PopoverTrigger asChild>
+                    <Badge variant="secondary" className="cursor-pointer px-2 text-base">
+                      {Array.isArray(label) ? label.join(' · ') : label}
+                    </Badge>
+                  </PopoverTrigger>
+                  <PopoverContent align="center" side="top" className="w-fit p-1.5 text-xs">
+                    <PopoverDescription>{key}</PopoverDescription>
+                  </PopoverContent>
+                </Popover>
+              ))}
+            </div>
           </div>
         </div>
       </section>
