caa224d0e5ca2afe7423c60f306a2296c1ed13f5
kimeodml <kimeodml@gmail.com>
2026-02-12 12:12:41 +0900

    feat: searchParams으로 step 관리

--- src/app/(app)/(with-back)/mypage/profile/basic/edit/page.tsx
+++ src/app/(app)/(with-back)/mypage/profile/basic/edit/page.tsx
@@ -1,10 +1,20 @@
 'use client';
 
-import { useSearchParams } from "next/navigation";
+import { useSearchParams } from 'next/navigation';
+
+import ChildrenStep from './ChildrenStep';
+import MaritalStatusStep from './MaritalStatusStep';
+import QuestionStep from './QuestionStep';
 
 export default function Page() {
   const searchParams = useSearchParams();
   const step = searchParams.get('step');
-  
-  return <div>수정페이지: {step} </div>;
+
+  return (
+    <div>
+      {step === 'marital-status' && <MaritalStatusStep />}
+      {step === 'children' && <ChildrenStep />}
+      {step === 'question' && <QuestionStep />}
+    </div>
+  );
 }
